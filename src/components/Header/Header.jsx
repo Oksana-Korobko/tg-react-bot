2e53ef7683b6c8170730642808e096448870c2de
Oksana <oksana.korobko.fd@gmail.com>
2024-05-23 14:15:07 +0300

    userName

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,9 @@ export default function Header() {
   return (
     <div className='header'>
         <Button onClick = {onClose}>Закрити</Button>
-        <span className='{username}'></span>
+        <span className='{username}'>
+          {tg.initDataUnsafe?.user?.username}
+        </span>
     </div>
   )
 }
