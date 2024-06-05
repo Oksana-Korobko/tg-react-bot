5f02302b28ea310fb065ebfacc47c0750cc12ae8
Oksana <oksana.korobko.fd@gmail.com>
2024-06-05 23:53:21 +0300

    22

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,62 +5,73 @@ import {useTelegram} from '../../hooks/useTelegram'
 export default function Form() {
 
   const [country, setCountry] = useState('');
-  const [street, setStreet] = useState('');
-  const [subject, setSubject] = useState('physical');
-  const {tg} = useTelegram();
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
+    const {tg} = useTelegram();
 
-  useEffect(() => {
-    tg.MainButton.setParams({
-        text: 'Отправить данные'
-    })
-  }, [])
+    // const onSendData = useCallback(() => {
+    //     const data = {
+    //         country,
+    //         street,
+    //         subject
+    //     }
+    //     tg.sendData(JSON.stringify(data));
+    // }, [country, street, subject])
 
-  // useEffect(() => {
-  //   if(!country||!street){
-  //     tg.MainButton.hide()
-  //   } else {
-  //     tg.MainButton.show()
-  //   }
-  // }, [country, street])
+    // useEffect(() => {
+    //     tg.onEvent('mainButtonClicked', onSendData)
+    //     return () => {
+    //         tg.offEvent('mainButtonClicked', onSendData)
+    //     }
+    // }, [onSendData])
 
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
 
-  const onChangeCountry = (e) => {
-      setCountry(e.target.value)
-  }
+    useEffect(() => {
+        if(!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street])
 
-  const onChangeStreet = (e) => {
-      setStreet(e.target.value)
-  }
+    const onChangeCountry = (e) => {
+        setCountry(e.target.value)
+    }
 
-  const onChangeSubject = (e) => {
-      setSubject(e.target.value)
-  }
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value)
+    }
 
+    const onChangeSubject = (e) => {
+        setSubject(e.target.value)
+    }
 
-  return (
-    
-    <div className={'form'}>
-     
-      <h3> Вкажіть ваші дані</h3>
-      <input 
-        className = {'input'} 
-        type='text' 
-        placeholder= {'Країна'} 
-        value={country}
-        onChange={onChangeCountry}
-      />
-      <input 
-        className = {'input'} 
-        type='text' 
-        placeholder= {'Вулиця'} 
-        value={street}
-        onChange={onChangeStreet}
-      />
-      <select value={subject} onChange={onChangeSubject} className={'select'}>
-        <option value={'physical'}>Фіз. особа</option>
-        <option value={'legal'}>Юр. особа</option>
-      </select>
-        
-    </div>
-  )
+    return (
+        <div className={"form"}>
+            <h3>Введите ваши данные</h3>
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
+            />
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ. лицо</option>
+                <option value={'legal'}>Юр. лицо</option>
+            </select>
+        </div>
+    );
 }
