9c4c119b23e8f9d28994d12297482cef9728e41b
Oksana <oksana.korobko.fd@gmail.com>
2024-06-05 16:04:10 +0300

    f2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -9,32 +9,32 @@ export default function Form() {
   const [subject, setSubject] = useState('physical');
   const {tg} = useTelegram();
 
-  useEffect(() => {
-    tg.MainButton.setParams({
-        text: 'Отправить данные'
-    })
-  }, [])
+  // useEffect(() => {
+  //   tg.MainButton.setParams({
+  //       text: 'Отправить данные'
+  //   })
+  // }, [])
 
-  useEffect(() => {
-    if(!country||!street){
-      tg.MainButton.hide()
-    } else {
-      tg.MainButton.show()
-    }
-  }, [country, street])
+  // useEffect(() => {
+  //   if(!country||!street){
+  //     tg.MainButton.hide()
+  //   } else {
+  //     tg.MainButton.show()
+  //   }
+  // }, [country, street])
 
 
-  const onChangeCountry = (e) => {
-      setCountry(e.target.value)
-  }
+  // const onChangeCountry = (e) => {
+  //     setCountry(e.target.value)
+  // }
 
-  const onChangeStreet = (e) => {
-      setStreet(e.target.value)
-  }
+  // const onChangeStreet = (e) => {
+  //     setStreet(e.target.value)
+  // }
 
-  const onChangeSubject = (e) => {
-      setSubject(e.target.value)
-  }
+  // const onChangeSubject = (e) => {
+  //     setSubject(e.target.value)
+  // }
 
 
   return (
