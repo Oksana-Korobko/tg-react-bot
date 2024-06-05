ecbdbd503686ca7fece425aad973db2f4ef6b377
Oksana <oksana.korobko.fd@gmail.com>
2024-06-05 16:12:27 +0300

    3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -9,39 +9,39 @@ export default function Form() {
   const [subject, setSubject] = useState('physical');
   const {tg} = useTelegram();
 
-  // useEffect(() => {
-  //   tg.MainButton.setParams({
-  //       text: 'Отправить данные'
-  //   })
-  // }, [])
+  useEffect(() => {
+    tg.MainButton.setParams({
+        text: 'Отправить данные'
+    })
+  }, [])
 
-  // useEffect(() => {
-  //   if(!country||!street){
-  //     tg.MainButton.hide()
-  //   } else {
-  //     tg.MainButton.show()
-  //   }
-  // }, [country, street])
+  useEffect(() => {
+    if(!country||!street){
+      tg.MainButton.hide()
+    } else {
+      tg.MainButton.show()
+    }
+  }, [country, street])
 
 
-  // const onChangeCountry = (e) => {
-  //     setCountry(e.target.value)
-  // }
+  const onChangeCountry = (e) => {
+      setCountry(e.target.value)
+  }
 
-  // const onChangeStreet = (e) => {
-  //     setStreet(e.target.value)
-  // }
+  const onChangeStreet = (e) => {
+      setStreet(e.target.value)
+  }
 
-  // const onChangeSubject = (e) => {
-  //     setSubject(e.target.value)
-  // }
+  const onChangeSubject = (e) => {
+      setSubject(e.target.value)
+  }
 
 
   return (
     
     <div className={'form'}>
-      Forma
-      {/* <h3> Вкажіть ваші дані</h3>
+     
+      <h3> Вкажіть ваші дані</h3>
       <input 
         className = {'input'} 
         type='text' 
@@ -59,7 +59,7 @@ export default function Form() {
       <select value={subject} onChange={onChangeSubject} className={'select'}>
         <option value={'physical'}>Фіз. особа</option>
         <option value={'legal'}>Юр. особа</option>
-      </select> */}
+      </select>
         
     </div>
   )
